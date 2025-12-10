6bfc81ce054decde02cd7ffe1c56c8730454032e
Isaac Azuelos <isaac@azuelos.ca>
2025-12-10 16:10:01 -0700

    Day 9, but slow
    I think we can compress the table to only care about points in the polygon, and then decompress the box after to compute the real area. This brings us from 100,000^2 to something like 250^2 which is more mangeable.

--- day9.py
+++ day9.py
@@ -41,49 +41,59 @@ def boxes_by_area(points):
 
 
 def box_to_points(box):
-    """
-    Turn our two-point box representation into a series of vertices like the
-    shape. This also is implicitly closed by looping back to the first point.
-    """
-    (p1, p2) = box
-    (x1, y1) = p1
-    (x2, y2) = p2
-    return [p1, (x1, y2), p2, (x2, y1)]
+    ((x1, y1), (x2, y2)) = box
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            yield (x, y)
+
+
+def on_edge(point, polygon):
+    px, py = point
+
+    for edge in polygon_edges(polygon):
+        ((x1, y1), (x2, y2)) = edge
+
+        if px == x1 and px == x2 and min(y1, y2) <= py and py <= max(y1, y2):
+            return True
+        if py == y1 and py == y2 and min(x1, x2) <= px and px <= max(x1, x2):
+            return True
+
+    return False
 
 
 def point_in_polygon(point, polygon):
     """https://en.wikipedia.org/wiki/Point_in_polygon"""
 
-    ray = (point, (0, point[1]))  # We cast our ray from the point straight left.
+    px, py = point
+
+    # we'll say our ray is is pointing to left.
 
     intersections = 0
     for edge in polygon_edges(polygon):
-        if rectilinear_line_segment_intersect(ray, edge):
-            intersections += 1
+        ((x1, y1), (x2, y2)) = edge
+        # On an edge means included in the polygon
+        if on_edge(point, edge):
+            return True
+
+        # check intersections on edges lines to the point's left.
+        # I'm not sure this is right???
+        if x1 < px and x2 < px:
+            # does the height of the ray to the left go between the line's top
+            # an bottom points?
+            if min(y1, y2) <= py and py <= max(y1, y2):
+                intersections += 1
 
     # odd means inside
     return (intersections % 2) == 1
 
 
 def polygon_edges(points):
-    last = points[0]
-    for point in points[1:]:
-        yield (last, point)
+
+    for i in range(len(points) - 1):
+        yield points[i], points[i + 1]
 
     # and it loops back around to close
     yield (points[-1], points[0])
-
-
-def is_horizontal(s):
-    return s[0][1] == s[1][1]
-
-
-def is_vertical(s):
-    return s[0][0] == s[1][0]
-
-
-def rectilinear_line_segment_intersect(l1, l2):
-    pass
 
 
 def box_in_polygon(box, polygon):
@@ -96,14 +106,7 @@ def box_in_polygon(box, polygon):
 
     for point in box_points:
         if not point_in_polygon(point, polygon):
-            print(f"rejecting {box_points} because {point} not in polygon")
             return False
-
-    for be in polygon_edges(box_points):
-        for pe in polygon_edges(polygon):
-            if rectilinear_line_segment_intersect(be, pe):
-                print(f"rejecting {box_points} because {be} intersects {pe}")
-                return False
 
     return True
 
@@ -119,29 +122,13 @@ def segments(points):
 
 
 def part2(points):
-    """
-    if all points in a polygon are inside another polygon, and none of the edges
-    intersect, then the first polygon is inside the other.
-
-
-    If all points of a shape corners are inside and none of the shapes' edges
-    intersect the box's edges, it's inside. Intersect here means non-parallel
-    intersection, which we can cheat on a bit since the lines are all
-    rectilinear.
-
-
-    https://en.wikipedia.org/wiki/Intersection_(geometry)#Two_line_segments
-    """
-
     # Area is cheap, so we find and sort by largest area, and then find the
     # first one that's inside (expensive), so we only need to consider one
     # to success, instead of every possible box to find the largest area.
 
     for box in boxes_by_area(points):
-        print(f"considering {box} with area {area(*box)}")
         if box_in_polygon(box, points):
-            # return area(*box)
-            print(f"box {box} has area {area(*box)}")
+            return area(*box)
 
 
 print("part 2:", part2(TEST))
